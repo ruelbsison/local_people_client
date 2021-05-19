6ffc1c9e4dfa9a63e6ff578df9318252c23e8198
Ruel Sison <rsjudge17@hotmail.com>
2021-05-19 14:06:13 +0800

    Apply changes on auth, profile ad job modules

--- lib/ui/views/main_screen.dart
+++ lib/ui/views/main_screen.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:local_people_core/core.dart';
 import 'home_client.dart';
-import 'more_screen.dart';
+import 'package:local_people_core/profile.dart';
 //import 'job_screen.dart';
 //import 'message_screen.dart';
 import 'search_screen.dart';
 import 'package:local_people_core/messages.dart';
 import 'package:local_people_core/jobs.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainScreen extends StatefulWidget {
   static Route route() {
@@ -48,32 +49,62 @@ class _MainScreenState extends State<MainScreen> {
           type: BottomNavigationBarType.fixed,
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Icon(
-                MaterialIcons.home,
+              // icon: Icon(
+              //   MaterialIcons.home,
+              // ),
+              icon: SvgPicture.asset(
+                'packages/local_people_core/assets/images/schedule-icon.svg'
+              ),
+              activeIcon: SvgPicture.asset(
+                  'packages/local_people_core/assets/images/schedule-active-icon.svg'
               ),
               label: LocalPeopleLocalizations.of(context).menuTitleHome,
             ),
             BottomNavigationBarItem(
-              icon: Icon(
-                MaterialIcons.work,
+              // icon: Icon(
+              //   MaterialIcons.work,
+              // ),
+              icon: SvgPicture.asset(
+                  'packages/local_people_core/assets/images/your-job-icon.svg'
+              ),
+              activeIcon: SvgPicture.asset(
+                'packages/local_people_core/assets/images/your-job-active-icon.svg'
               ),
               label: LocalPeopleLocalizations.of(context).menuTitleYourJobs,
             ),
             BottomNavigationBarItem(
-              icon: Icon(
-                MaterialIcons.search,
+              // icon: Icon(
+              //   MaterialIcons.search,
+              // ),
+              icon: SvgPicture.asset(
+                  'packages/local_people_core/assets/images/search-icon.svg'
+              ),
+              activeIcon: SvgPicture.asset(
+                  'packages/local_people_core/assets/images/search-active-icon.svg'
               ),
               label: LocalPeopleLocalizations.of(context).menuTitleSearch,
             ),
             BottomNavigationBarItem(
-              icon: Icon(
-                MaterialIcons.message,
+              // icon: Icon(
+              //   MaterialIcons.message,
+              // ),
+              icon: SvgPicture.asset(
+                  'packages/local_people_core/assets/images/messages-icon.svg'
+              ),
+              activeIcon: SvgPicture.asset(
+                  'packages/local_people_core/assets/images/messages-active-icon.svg'
               ),
               label: LocalPeopleLocalizations.of(context).menuTitleMessages,
             ),
             BottomNavigationBarItem(
-              icon: Icon(
-                MaterialIcons.menu,
+              // icon: Icon(
+              //   MaterialIcons.menu,
+              // ),
+              icon: SvgPicture.asset(
+                  'packages/local_people_core/assets/images/more-icon.svg'
+              ),
+              activeIcon: SvgPicture.asset(
+                  'packages/local_people_core/assets/images/more-active-icon.svg'
               ),
               label: LocalPeopleLocalizations.of(context).menuTitleMore,
             ),
